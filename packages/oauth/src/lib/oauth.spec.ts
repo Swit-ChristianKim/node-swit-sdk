748154e011dc2b0639c4cde955752f71854f4f62
Christian Kim <christian@swit.io>
2023-09-16 16:25:29 +0900

    chore: oauth 프로젝트 생성

---
+++ packages/oauth/src/lib/oauth.spec.ts
@@ -0,0 +1,7 @@
+import { oauth } from './oauth';
+
+describe('oauth', () => {
+  it('should work', () => {
+    expect(oauth()).toEqual('oauth');
+  });
+});
---
+++ packages/oauth/src/lib/oauth.ts
@@ -0,0 +1,3 @@
+export function oauth(): string {
+  return 'oauth';
+}
