f0e8c4d167c4ac633b12910f50fe3a0710bb0682
Christian Kim <christian@swit.io>
2023-11-09 06:54:58 +0900

    chore: change style of docusaurus.

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,12 +5,12 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--dark', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/getting-started/introduction">
+            Getting started
           </Link>
         </div>
       </div>
