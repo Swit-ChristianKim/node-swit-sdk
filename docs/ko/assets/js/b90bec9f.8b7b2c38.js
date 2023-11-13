10b60f86c80f62405a20eebc10d06ac61522a796
Christian Kim <christian@swit.io>
2023-11-14 03:17:50 +0900

    chore: change workflow.

--- docs/ko/assets/js/b90bec9f.8b7b2c38.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[8062],{3033:e=>{e.exports=JSON.parse('{"permalink":"/node-swit-sdk/ko/blog/tags/hello","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
